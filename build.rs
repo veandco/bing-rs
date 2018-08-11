d1830a07c958a015a43a5d39aa2c554cf094f942
Lilis Iskandar <lilis@veand.co>
2018-08-11 23:34:10 +0000

    build(build.rs): Write generated header file into $OUT_DIR
    Signed-off-by: Lilis Iskandar <lilis@veand.co>

--- build.rs
+++ build.rs
@@ -2,9 +2,11 @@ extern crate cbindgen;
 
 // std
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     cbindgen::Builder::new()
         .with_crate(crate_dir)
@@ -12,5 +14,5 @@ fn main() {
         .with_include_guard("bing_rs_h")
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("bing-rs.h");
+        .write_to_file(out_path.join("bing-rs.h"));
 }
